9e9c8c6ce568f53c5e2231e7decbe6660eda9bfa
abhijit barick <barick.abhijeet@gmail.com>
2025-11-17 01:22:16 +0530

    update header animation

--- src/components/Header/NavigationLinks.tsx
+++ src/components/Header/NavigationLinks.tsx
@@ -3,14 +3,15 @@ import { clsx } from "clsx";
 import Link from "next/link";
 import React from "react";
 
-export const NavigationLinks: React.FC<{ className?: string, onClick?: () => void }> = ({
+export const NavigationLinks: React.FC<{ className?: string, onClick?: () => void, isDropdownOpen?: boolean }> = ({
   className,
-  onClick
+  onClick,
+  isDropdownOpen
 }) => {
   return (
-    <ul className={clsx("flex gap-8", className)}>
+    <ul className={clsx("flex gap-5", className)}>
       {headerConstants.menu.links.map((link, i) => (
-        <li key={i}>
+        <li key={i} className={clsx(isDropdownOpen && "animate-slide-up", `menu-item-${i}`)}>
           <Link href={link.href} className="text-white" onClick={onClick}>
             {link.label}
           </Link>
